895785bfb30c2f97896dc8990cc41472f333e4d4
Ahmed-Elshhat <a7medelshhat@gmail.com>
2024-11-24 01:23:31 +0200

    "edit navbar"

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,15 +2,50 @@ import { NavLink } from "react-router-dom";
 import "./NavBar.scss";
 
 function NavBar() {
-  return <nav className="navbar">
-    <div className="logo">LOGO</div>
-    <ul>
-      <li><NavLink to="/">Home</NavLink></li>
-      <li><NavLink to="/about">About us</NavLink></li>
-      <li><NavLink to="/service">Services</NavLink></li>
-      <li><NavLink to="/contact">Contact us</NavLink></li>
-    </ul>
-  </nav>;
+  return (
+    <nav className="navbar">
+      <div className="logo">LOGO</div>
+
+      <dv className="links">
+        <span className="icon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <ul>
+          <li>
+            <NavLink to="/">
+              Home
+              <span></span>
+              <span></span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">
+              About us
+              <span></span>
+              <span></span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/service">
+              Services
+              <span></span>
+              <span></span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">
+              Contact us
+              <span></span>
+              <span></span>
+            </NavLink>
+          </li>
+        </ul>
+      </dv>
+    </nav>
+  );
 }
 
 export default NavBar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,15 @@
 import "./Home.scss";
 import NavBar from "../../components/NavBar/NavBar";
-import pathImgs from "../../images/path"
+import pathImgs from "../../images/path";
 
 function Home() {
   return (
     <div className="Home">
       <header>
-        <img src={pathImgs.shadowImg} alt=""/>
+        <img src={pathImgs.shadowImg} alt="" />
         <NavBar />
         <div className="content">
-          <div className="left-side">
-
-          </div>
+          <div className="left-side"></div>
         </div>
       </header>
     </div>
