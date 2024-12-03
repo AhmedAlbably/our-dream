829cc3e8c8e7adb5ecd50ffcd4e7064c501ecb58
Ahmed-Elshhat <a7medelshhat@gmail.com>
2024-12-03 19:25:13 +0200

    finally

--- client/src/images/path.js
+++ client/src/images/path.js
@@ -4,6 +4,7 @@ import backgroundOne from "./backgroundOne.png";
 import userImg from "./userTwo.png";
 import quote from "./quote.png";
 import ServicesBanner from "./ServicesBanner.png";
+import aboutUsImg from "./aboutUsImg.png";
 
 const pathImgs = {
   shadowImg,
@@ -11,7 +12,8 @@ const pathImgs = {
   backgroundOne,
   userImg,
   quote,
-  ServicesBanner
+  ServicesBanner,
+  aboutUsImg
 };
 
 export default pathImgs;
--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,5 +1,6 @@
 import NavBar from "../../components/NavBar/NavBar";
 import "./About.scss";
+import pathImgs from "../../images/path";
 
 function About() {
   return (
@@ -15,14 +16,26 @@ function About() {
           achieve success and innovation easily.
         </p>
       </header>
+
+      <section className="about-us">
+        <div className="about-us-text">
+          <h2>About Us <span></span></h2>
+          <h3>A platform that combines creativity and achievement</h3>
+          <p>
+            It is a global freelancing platform that connects project owners
+            with freelancers to perform tasks such as programming, design, and
+            writing, with a secure payment system and a huge community of users.
+          </p>
+        </div>
+
+        <div className="about-us-img">
+          <div className="image">
+          <img src={pathImgs.aboutUsImg} alt="" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
 
 export default About;
-
-
-
-
-
-
